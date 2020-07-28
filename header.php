449bb19d37f987f33e05f00b0b44992565ca09cf
Sean Cloudsdale <sean@Seans-MacBook-Pro.local>
2020-07-28 14:56:59 +0100

    align nav items right

--- header.php
+++ header.php
@@ -30,15 +30,16 @@
 
 	<nav class="navbar fixed-top navbar-expand-lg navbar-dark blue scrolling-navbar" role="navigation">
   <div class="container">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
+	<!-- Brand and toggle get grouped for better mobile display -->
 	<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
 			<strong><?php bloginfo( 'name' ); ?></strong>
 	
 		</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+	
 
 	
         <?php
@@ -46,13 +47,15 @@
             'theme_location'    => 'primary',
             'depth'             => 2,
             'container'         => 'div',
-            'container_class'   => 'collapse navbar-collapse',
+            'container_class'   => 'collapse navbar-collapse justify-content-end',
             'container_id'      => 'bs-example-navbar-collapse-1',
             'menu_class'        => 'nav navbar-nav',
             'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
             'walker'            => new WP_Bootstrap_Navwalker(),
         ) );
-        ?>
+		?>
+		
+		
     </div>
 </nav>
 		
